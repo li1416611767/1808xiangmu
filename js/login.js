a27803ee40dcfa2c9c5c732873c1ec79a148c827
li1416611767 <1416611767@qq.com>
2019-05-27 20:43:48 +0800

    '项目'

---
+++ js/login.js
@@ -0,0 +1,87 @@
+(function($){
+        //手机号正则验证
+        var reg2 = /^1[34578]{1}\d{9}$/;
+		$('#phone').blur(function(){
+	    if(reg2.test(phone.value)){
+		  phone.style.borderBottom="1px solid #ccc";
+	    }else{
+		  phone.style.borderBottom="3px solid #ff6d6d";
+		}
+	    })
+	   //获取验证码
+	   var off=true;
+        var arr=new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+		function obtainCode() {
+        value="";
+        for(var i=0;i<+4;i++){
+            var index=Math.floor(Math.random()*arr.length);
+            value+=arr[index]
+          }
+        }
+		obtainCode();
+		$('.butt').click(function(){
+			$.ajax({
+			url:'php/index.php?c=Login&a=query1',
+			type:'post',
+			data:{
+				page:$(phone).val(),
+			},
+			success:function(data){
+				var str=JSON.parse(data)
+				console.log(str)
+				if (str.code=='200') {
+					off=false;
+				}
+				else{
+					off=true;
+				}
+			}
+		})
+			if ($('#phone').val()=='') {
+            alert('手机号码不能为空')
+		}else(alert(value))
+		return
+		})
+
+		$('.nums').onblur=function(){
+			alert(1)
+		}
+  
+        $('.denglu').click(function(){
+		var transCode=code.value.toUpperCase();
+		if(yz.value.length==0){
+            alert("验证码不能为空");
+        }
+        yz.value="";
+		})
+
+
+
+
+
+		$('.denglu').click(function(){
+		if (off) {
+		$.ajax({
+			url:'php/index.php?c=Login&a=register1',
+			type:'post',
+			data:{
+				username:$('#phone').val(),
+			},
+			success:function(data){
+				console.log(data)
+                var str=JSON.parse(data);
+                console.log(str)
+                if (str.code=='200') {
+					localStorage.username=$('#phone').val();
+                	alert('成功录入')
+                }
+                
+			}
+		})
+	}
+	else{
+		alert('成功录入')
+	}
+	})
+
+})(jQuery)
--- php/LoginController.class.php
+++ php/LoginController.class.php
@@ -22,6 +22,32 @@ class LoginController{
                     exit;
                 }
         }
+        public function register1(){
+            //if(IS_AJAX) {
+                $data = $_POST;
+                $username = $data["username"];
+                    $result = M()->add("user", $data);
+                    if ($result) {
+                        echo ajax_return("200", "插入成功", "");
+                        exit;
+                    } else {
+                        echo ajax_return("404", "插入失败", "");
+                        exit;
+                    }
+            }
+    
+            public function query1(){
+                $data = $_POST;
+                $page = $data["page"];
+                $old_data1 = M()->query_sql("SELECT * FROM user WHERE username={$page}");
+                $counta=count($old_data1);
+                if ($counta) {
+                    echo ajax_return('200','',$old_data1);
+                }
+                else{
+                    echo ajax_return('404','',$old_data1);
+                }
+            }
     /*
      * 加密方法
      */
